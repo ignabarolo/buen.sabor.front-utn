fa8df1f720147d7ab59b5bc7cf5ceb9370eedd3c
NicoCataldo <101023955+NicoCataldo@users.noreply.github.com>
2024-04-29 17:35:31 -0300

    Back

---
+++ Practicas/src/main/java/com/example/Practicas/PracticasApplication.java
@@ -0,0 +1,14 @@
+package com.example.Practicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasApplication.class, args);
+		System.out.println("Funcionando");
+	}
+
+}
