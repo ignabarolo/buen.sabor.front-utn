217501589fbb239afb58ff222e36306cb87768a7
NicoCataldo <101023955+NicoCataldo@users.noreply.github.com>
2024-04-30 16:48:46 -0300

    BorrarPraacticas

--- Practicas/src/main/java/com/example/Practicas/repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Practicas.repository;
-
-import com.example.Practicas.model.Persona;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonaRepository extends BaseRepository<Persona, Long>{
-}
--- Practicas/src/main/java/com/example/Practicas/service/PersonaService.java
+++
@@ -1,7 +0,0 @@
-package com.example.Practicas.service;
-
-
-import com.example.Practicas.model.Persona;
-
-public interface PersonaService extends BaseService<Persona, Long>{
-}
